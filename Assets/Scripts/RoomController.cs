ecfc55ccf2c859dd2781154ae3cb8763f2c92d84
Brandon <bmc180001@utdallas.edu>
2020-07-22 23:06:45 -0500

    Fixed Maya Scene

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -4,8 +4,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RoomController : MonoBehaviour
-{
+public class RoomController : MonoBehaviour {
     public string level;
     public float roomSize = 10f;
     public float offX = 0f;
@@ -16,93 +15,79 @@ public class RoomController : MonoBehaviour
     ImageController imageController;
     public List<GameObject> startingRooms;
     public List<GameObject> availableRooms;
-    public List<Room> activeRooms = new List<Room>();
+    public List<Room> activeRooms = new List<Room> ();
 
-    private void Awake()
-    {
+    private void Awake () {
         int displacement = 0;
-        while (startingRooms.Count > 0)
-        {
-            GenerateRoom(displacement++, 0, startingRooms[0], "Ignore");
-            startingRooms.RemoveAt(0);
+        while (startingRooms.Count > 0) {
+            GenerateRoom (displacement++, 0, startingRooms[0], "Ignore");
+            startingRooms.RemoveAt (0);
         }
-        imageController = GameObject.Find("ImageControl").GetComponent<ImageController>();
+        imageController = GameObject.Find ("ImageControl").GetComponent<ImageController> ();
     }
-    bool DoesRoomExist(int x, int z)
-    {
-        return !(activeRooms.Find(item => item.X == x && item.Z == z) == null);
+    bool DoesRoomExist (int x, int z) {
+        return !(activeRooms.Find (item => item.X == x && item.Z == z) == null);
     }
-    public int AttemptRoomGeneration(Room room, int dir)
-    {
-        if (availableRooms.Count == 0)
-        {
-            Debug.Log("No available rooms left!");
+    public int AttemptRoomGeneration (Room room, int dir) {
+        if (availableRooms.Count == 0) {
+            Debug.Log ("No available rooms left!");
             return 0;
         }
         int newRoomIndex;
-        switch (dir)
-        {
+        switch (dir) {
             //Dir rotates incrementally clockwise
             //Dir: 0 = PosZ, 1 = PosX, 2 = NegZ, 3 = NegX
             case 0:
-                if (DoesRoomExist(room.X, room.Z + 1))
-                {
-                    Debug.Log("Attempted Room Generation where Room Exists");
+                if (DoesRoomExist (room.X, room.Z + 1)) {
+                    Debug.Log ("Attempted Room Generation where Room Exists");
                     return 2;
                 }
-                newRoomIndex = Mathf.FloorToInt(Random.Range(0, availableRooms.Count - 0.1f));
-                GenerateRoom(room.X, room.Z + 1, availableRooms[newRoomIndex], "PosZ");
-                availableRooms.RemoveAt(newRoomIndex);
+                newRoomIndex = Mathf.FloorToInt (Random.Range (0, availableRooms.Count - 0.1f));
+                GenerateRoom (room.X, room.Z + 1, availableRooms[newRoomIndex], "PosZ");
+                availableRooms.RemoveAt (newRoomIndex);
                 return 1;
             case 1:
-                if (DoesRoomExist(room.X + 1, room.Z))
-                {
-                    Debug.Log("Attempted Room Generation where Room Exists");
+                if (DoesRoomExist (room.X + 1, room.Z)) {
+                    Debug.Log ("Attempted Room Generation where Room Exists");
                     return 2;
                 }
-                newRoomIndex = Mathf.FloorToInt(Random.Range(0, availableRooms.Count - 0.1f));
-                GenerateRoom(room.X + 1, room.Z, availableRooms[newRoomIndex], "PosX");
-                availableRooms.RemoveAt(newRoomIndex);
+                newRoomIndex = Mathf.FloorToInt (Random.Range (0, availableRooms.Count - 0.1f));
+                GenerateRoom (room.X + 1, room.Z, availableRooms[newRoomIndex], "PosX");
+                availableRooms.RemoveAt (newRoomIndex);
                 return 1;
             case 2:
-                if (DoesRoomExist(room.X, room.Z - 1))
-                {
-                    Debug.Log("Attempted Room Generation where Room Exists");
+                if (DoesRoomExist (room.X, room.Z - 1)) {
+                    Debug.Log ("Attempted Room Generation where Room Exists");
                     return 2;
                 }
-                newRoomIndex = Mathf.FloorToInt(Random.Range(0, availableRooms.Count - 0.1f));
-                GenerateRoom(room.X, room.Z - 1, availableRooms[newRoomIndex], "NegZ");
-                availableRooms.RemoveAt(newRoomIndex);
+                newRoomIndex = Mathf.FloorToInt (Random.Range (0, availableRooms.Count - 0.1f));
+                GenerateRoom (room.X, room.Z - 1, availableRooms[newRoomIndex], "NegZ");
+                availableRooms.RemoveAt (newRoomIndex);
                 return 1;
             case 3:
-                if (DoesRoomExist(room.X - 1, room.Z))
-                {
-                    Debug.Log("Attempted Room Generation where Room Exists");
+                if (DoesRoomExist (room.X - 1, room.Z)) {
+                    Debug.Log ("Attempted Room Generation where Room Exists");
                     return 2;
                 }
-                newRoomIndex = Mathf.FloorToInt(Random.Range(0, availableRooms.Count - 0.1f));
-                GenerateRoom(room.X - 1, room.Z, availableRooms[newRoomIndex], "NegX");
-                availableRooms.RemoveAt(newRoomIndex);
+                newRoomIndex = Mathf.FloorToInt (Random.Range (0, availableRooms.Count - 0.1f));
+                GenerateRoom (room.X - 1, room.Z, availableRooms[newRoomIndex], "NegX");
+                availableRooms.RemoveAt (newRoomIndex);
                 return 1;
             default:
-                Debug.Log("Invalid Direction for Room Generation");
+                Debug.Log ("Invalid Direction for Room Generation");
                 return 0;
         }
     }
-    int RoomRotateTest(string dir, Room newRoom)
-    {
-        if (newRoom.doors.Count == 0)
-        {
+    int RoomRotateTest (string dir, Room newRoom) {
+        if (newRoom.doors.Count == 0) {
             return 0;
         }
-        List<Door> doorScripts = new List<Door>();
-        foreach(GameObject door in newRoom.doors)
-        {
-            doorScripts.Add(door.GetComponent<Door>());
+        List<Door> doorScripts = new List<Door> ();
+        foreach (GameObject door in newRoom.doors) {
+            doorScripts.Add (door.GetComponent<Door> ());
         }
         int incoming;
-        switch(dir)
-        {
+        switch (dir) {
             case "PosZ":
                 incoming = 2;
                 break;
@@ -118,46 +103,38 @@ public class RoomController : MonoBehaviour
             case "Ignore":
                 return 0;
             default:
-                Debug.Log("Invalid Direction for Room Rotation Test");
+                Debug.Log ("Invalid Direction for Room Rotation Test");
                 return 0;
         }
         int rotation = 0;
-        while (true)
-        {
-            foreach (Door door in doorScripts)
-            {
+        while (true) {
+            foreach (Door door in doorScripts) {
                 int newRotation = door.facing + rotation;
-                while (newRotation > 3)
-                {
+                while (newRotation > 3) {
                     newRotation -= 4;
                 }
-                if (newRotation == incoming)
-                {
-                    Destroy(door.gameObject);
+                if (newRotation == incoming) {
+                    Destroy (door.gameObject);
                     return rotation;
                 }
             }
             rotation++;
-            if (rotation > 3)
-            {
-                Debug.Log("Invalid Rotation for Room Rotation Test");
+            if (rotation > 3) {
+                Debug.Log ("Invalid Rotation for Room Rotation Test");
                 return 0;
             }
         }
     }
-    void GenerateDoor(Room room, string doorDir)
-    {
-        Transform doorGroup = room.model.transform.Find("Door");
-        if (doorGroup.Find("Door" + doorDir))
-        {
-            GameObject door = doorGroup.Find("Door" + doorDir).gameObject;
-            MeshCollider bad = door.GetComponent<MeshCollider>();
+    void GenerateDoor (Room room, string doorDir) {
+        Transform doorGroup = room.model.transform.Find ("Door");
+        if (doorGroup.Find ("Door" + doorDir)) {
+            GameObject door = doorGroup.Find ("Door" + doorDir).gameObject;
+            MeshCollider bad = door.GetComponent<MeshCollider> ();
             bad.enabled = false;
-            BoxCollider doorActivation = door.AddComponent<BoxCollider>();
+            BoxCollider doorActivation = door.AddComponent<BoxCollider> ();
             doorActivation.isTrigger = true;
-            Door doorScript = door.AddComponent<Door>();
-            switch(doorDir)
-            {
+            Door doorScript = door.AddComponent<Door> ();
+            switch (doorDir) {
                 //Dir rotates incrementally clockwise
                 //Dir: 0 = PosZ, 1 = PosX, 2 = NegZ, 3 = NegX
                 case "PosZ":
@@ -173,94 +150,75 @@ public class RoomController : MonoBehaviour
                     doorScript.facing = 3;
                     break;
                 default:
-                    Debug.Log("Invalid  Direction for Door Generation");
+                    Debug.Log ("Invalid  Direction for Door Generation");
                     break;
             }
             doorScript.room = room;
             doorScript.controlScript = this;
-            room.doors.Add(door);
+            room.doors.Add (door);
         }
     }
-    void GenerateCard(Room room)
-    {
+    void GenerateCard (Room room) {
         GameObject newCard;
         CardInRoom cardScript;
         Light cardHalo;
-        if (room.model.transform.Find("Event"))
-        {
-            newCard = room.model.transform.Find("Event").gameObject;
-            cardScript = newCard.AddComponent<CardInRoom>();
-            if (imageController.Events.Count > 0)
-            {
-                int index = Random.Range(0, imageController.Events.Count);
+        if (room.model.transform.Find ("Event")) {
+            newCard = room.model.transform.Find ("Event").gameObject;
+            cardScript = newCard.AddComponent<CardInRoom> ();
+            if (imageController.Events.Count > 0) {
+                int index = Random.Range (0, imageController.Events.Count);
                 cardScript.cardImage = imageController.Events[index];
                 cardScript.cardType = "Event";
-                imageController.Events.RemoveAt(index);
+                imageController.Events.RemoveAt (index);
+            } else {
+                Debug.Log ("No Event Cards Left!");
             }
-            else
-            {
-                Debug.Log("No Event Cards Left!");
-            }
-            cardHalo = newCard.AddComponent<Light>();
+            cardHalo = newCard.AddComponent<Light> ();
             cardHalo.color = Color.blue;
-        }
-        else if (room.model.transform.Find("Item"))
-        {
-            newCard = room.model.transform.Find("Item").gameObject;
-            cardScript = newCard.AddComponent<CardInRoom>();
-            if (imageController.Items.Count > 0)
-            {
-                int index = Random.Range(0, imageController.Items.Count);
+        } else if (room.model.transform.Find ("Item")) {
+            newCard = room.model.transform.Find ("Item").gameObject;
+            cardScript = newCard.AddComponent<CardInRoom> ();
+            if (imageController.Items.Count > 0) {
+                int index = Random.Range (0, imageController.Items.Count);
                 cardScript.cardImage = imageController.Items[index];
                 cardScript.cardType = "Item";
-                imageController.Items.RemoveAt(index);
+                imageController.Items.RemoveAt (index);
+            } else {
+                Debug.Log ("No Item Cards Left!");
             }
-            else
-            {
-                Debug.Log("No Item Cards Left!");
-            }
-            cardHalo = newCard.AddComponent<Light>();
+            cardHalo = newCard.AddComponent<Light> ();
             cardHalo.color = Color.yellow;
-        }
-        else if (room.model.transform.Find("Omen"))
-        {
-            newCard = room.model.transform.Find("Omen").gameObject;
-            cardScript = newCard.AddComponent<CardInRoom>();
-            if (imageController.Omens.Count > 0)
-            {
-                int index = Random.Range(0, imageController.Omens.Count);
+        } else if (room.model.transform.Find ("Omen")) {
+            newCard = room.model.transform.Find ("Omen").gameObject;
+            cardScript = newCard.AddComponent<CardInRoom> ();
+            if (imageController.Omens.Count > 0) {
+                int index = Random.Range (0, imageController.Omens.Count);
                 cardScript.cardImage = imageController.Omens[index];
                 cardScript.cardType = "Omen";
-                imageController.Omens.RemoveAt(index);
+                imageController.Omens.RemoveAt (index);
+            } else {
+                Debug.Log ("No Omen Cards Left!");
             }
-            else
-            {
-                Debug.Log("No Omen Cards Left!");
-            }
-            cardHalo = newCard.AddComponent<Light>();
+            cardHalo = newCard.AddComponent<Light> ();
             cardHalo.color = Color.green;
-        }
-        else
-        {
+        } else {
             return;
         }
         cardHalo.type = LightType.Point;
         cardHalo.intensity = 4;
         cardHalo.range = 4;
-        MeshCollider badCollider = newCard.GetComponent<MeshCollider>();
+        MeshCollider badCollider = newCard.GetComponent<MeshCollider> ();
         badCollider.enabled = false;
-        SphereCollider newCollider = newCard.AddComponent<SphereCollider>();
+        SphereCollider newCollider = newCard.AddComponent<SphereCollider> ();
         newCollider.isTrigger = true;
     }
-    void GenerateRoom(int x, int z, GameObject referenceRoom, string dir)
-    {
-        string one = referenceRoom.name.Substring(4, 1);
-        string two = referenceRoom.name.Substring(5, 1);
-        if (one == "0")
-        {
+    void GenerateRoom (int x, int z, GameObject referenceRoom, string dir) {
+        string one = referenceRoom.name.Substring (4, 1);
+        string two = referenceRoom.name.Substring (5, 1);
+        if (one == "0") {
             one = "";
         }
-        Sprite mapTile;
+        //Sprite mapTile;
         //Debug.Log(imageController.Map.Count);
         //foreach (Sprite tile in imageController.Map)
         //Debug.Log(imageController.Map.Count);
@@ -269,56 +227,49 @@ public class RoomController : MonoBehaviour
             //Debug.Log(imageController.Map[i]);
             //if (tile.name == "Room_" + string.Concat(one, two))
         }
-        Room newRoom = new Room
-        {
+        Room newRoom = new Room {
             name = referenceRoom.name,
             modelPrefab = referenceRoom,
-            model = Instantiate(referenceRoom),
+            model = Instantiate (referenceRoom),
             X = x,
             Z = z,
             rotation = 0,
-            doors = new List<GameObject>(),
+            doors = new List<GameObject> (),
             //tile = mapTile
         };
-        activeRooms.Add(newRoom);
-        GenerateDoor(newRoom, "PosX");
-        GenerateDoor(newRoom, "NegX");
-        GenerateDoor(newRoom, "PosZ");
-        GenerateDoor(newRoom, "NegZ");
-        GenerateCard(newRoom);
-        int rotations = RoomRotateTest(dir, newRoom);
+        activeRooms.Add (newRoom);
+        GenerateDoor (newRoom, "PosX");
+        GenerateDoor (newRoom, "NegX");
+        GenerateDoor (newRoom, "PosZ");
+        GenerateDoor (newRoom, "NegZ");
+        GenerateCard (newRoom);
+        int rotations = RoomRotateTest (dir, newRoom);
         newRoom.rotation = rotations;
         newRoom.model.transform.parent = this.transform;
-        newRoom.model.transform.position = new Vector3(x * roomSize + offX, offY, z * roomSize + offZ);
-        newRoom.model.transform.rotation = Quaternion.Euler(0f, 90f * rotations, 0f);
-        while (rotations > 0)
-        {
-            foreach (GameObject door in newRoom.doors)
-            {
-                door.GetComponent<Door>().RotateClockwise();
+        newRoom.model.transform.position = new Vector3 (x * roomSize + offX, offY, z * roomSize + offZ);
+        newRoom.model.transform.rotation = Quaternion.Euler (0f, 90f * rotations, 0f);
+        while (rotations > 0) {
+            foreach (GameObject door in newRoom.doors) {
+                door.GetComponent<Door> ().RotateClockwise ();
             }
             rotations--;
         }
-        GameObject lightGameObject = new GameObject(newRoom.name + " Light");
-        Light lightComp = lightGameObject.AddComponent<Light>();
-        lightComp.color = Color.Lerp(Color.red, Color.green, 0.4f);
+        GameObject lightGameObject = new GameObject (newRoom.name + " Light");
+        Light lightComp = lightGameObject.AddComponent<Light> ();
+        lightComp.color = Color.Lerp (Color.red, Color.green, 0.4f);
         lightComp.intensity = 20f;
-        lightGameObject.transform.position = new Vector3(x * roomSize + offX, 2 * roomSize / 5 + offY, z * roomSize + offZ);
+        lightGameObject.transform.position = new Vector3 (x * roomSize + offX, 2 * roomSize / 5 + offY, z * roomSize + offZ);
 
-        RoomController otherControl1 = otherFloor1.GetComponent<RoomController>();
-        for (int i = 0; i < otherControl1.availableRooms.Count; i++)
-        {
-            if (otherControl1.availableRooms[i].name == referenceRoom.name)
-            {
-                otherControl1.availableRooms.RemoveAt(i);
+        RoomController otherControl1 = otherFloor1.GetComponent<RoomController> ();
+        for (int i = 0; i < otherControl1.availableRooms.Count; i++) {
+            if (otherControl1.availableRooms[i].name == referenceRoom.name) {
+                otherControl1.availableRooms.RemoveAt (i);
             }
         }
-        RoomController otherControl2 = otherFloor1.GetComponent<RoomController>();
-        for (int i = 0; i < otherControl2.availableRooms.Count; i++)
-        {
-            if (otherControl2.availableRooms[i].name == referenceRoom.name)
-            {
-                otherControl2.availableRooms.RemoveAt(i);
+        RoomController otherControl2 = otherFloor1.GetComponent<RoomController> ();
+        for (int i = 0; i < otherControl2.availableRooms.Count; i++) {
+            if (otherControl2.availableRooms[i].name == referenceRoom.name) {
+                otherControl2.availableRooms.RemoveAt (i);
             }
         }
     }
